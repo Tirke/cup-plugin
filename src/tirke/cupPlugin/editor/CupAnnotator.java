d06f94197889ac8f08c94a7a8ea07d72a74952ef
Tirke <toj315@gmail.com>
2016-02-23 00:56:17 +0100

    New annotator using visit methods

--- src/tirke/cupPlugin/editor/CupAnnotator.java
+++ src/tirke/cupPlugin/editor/CupAnnotator.java
@@ -2,11 +2,13 @@ package tirke.cupPlugin.editor;
 
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import tirke.cupPlugin.highlighting.CupSyntaxHighlighter;
 import tirke.cupPlugin.psi.CupLabelId;
 import tirke.cupPlugin.psi.CupRuleId;
+import tirke.cupPlugin.psi.CupVisitor;
 
 /**
  * Created by Tirke on 19/02/2016
@@ -14,12 +16,24 @@ import tirke.cupPlugin.psi.CupRuleId;
 public class CupAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-        PsiElement parent = element.getParent();
 
-        if (parent instanceof CupLabelId) {
-            holder.createInfoAnnotation(element, null).setTextAttributes(CupSyntaxHighlighter.LABEL_ID);
-        } else if (parent instanceof CupRuleId) {
-            holder.createInfoAnnotation(element, null).setTextAttributes(CupSyntaxHighlighter.RULE_ID);
-        }
+        element.accept(new CupVisitor() {
+            @Override
+            public void visitLabelId(@NotNull CupLabelId o) {
+                super.visitLabelId(o);
+                setHighlighting(o, holder, CupSyntaxHighlighter.LABEL_ID);
+            }
+
+            @Override
+            public void visitRuleId(@NotNull CupRuleId o) {
+                super.visitRuleId(o);
+                setHighlighting(o, holder, CupSyntaxHighlighter.RULE_ID);
+            }
+        });
+    }
+
+    private static void setHighlighting(@NotNull PsiElement element, @NotNull AnnotationHolder holder,
+                                        @NotNull TextAttributesKey key) {
+        holder.createInfoAnnotation(element, null).setTextAttributes(key);
     }
 }
