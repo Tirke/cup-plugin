cf78612424dc361e481ff25de795e3a803fc4c41
Tirke <toj315@gmail.com>
2016-02-24 00:04:44 +0100

    Commenter

---
+++ src/tirke/cupPlugin/editor/CupCommenter.java
@@ -0,0 +1,39 @@
+package tirke.cupPlugin.editor;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created by Tirke on 23/02/2016
+ */
+public class CupCommenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "//";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "/*";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return "*/";
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
